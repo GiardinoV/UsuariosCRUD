d96143f71363d9157df8f6747a290137e3a830ae
virginia <vgiardino@outlook.es>
2022-02-14 18:20:22 -0300

    Conecto con base de datos

---
+++ apiRest/src/main/java/com/apiResrt/apiRest/Models/UsuarioModel.java
@@ -0,0 +1,49 @@
+package com.apiResrt.apiRest.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String nomber;
+    private String email;
+    private Integer prioridad;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomber() {
+        return nomber;
+    }
+
+    public void setNomber(String nomber) {
+        this.nomber = nomber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+}
