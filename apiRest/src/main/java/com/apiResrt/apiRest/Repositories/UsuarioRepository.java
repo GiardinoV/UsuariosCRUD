ae5cf259f30683b7387a1866aff746d4a4135675
virginia <vgiardino@outlook.es>
2022-02-15 11:41:32 -0300

    Creo metodos faltantes y cambio puerto a 8090

--- apiRest/src/main/java/com/apiResrt/apiRest/Repositories/UsuarioRepository.java
+++ apiRest/src/main/java/com/apiResrt/apiRest/Repositories/UsuarioRepository.java
@@ -4,6 +4,9 @@ import com.apiResrt.apiRest.Models.UsuarioModel;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long> {
+    public abstract ArrayList<UsuarioModel> findByPrioridad(Integer prioridad);
 }
