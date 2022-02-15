be7b2f7c98f8f97ab2e257d330fa92c0f7170884
virginia <vgiardino@outlook.es>
2022-02-15 10:19:32 -0300

    Creo los metodos

--- apiRest/src/main/java/com/apiResrt/apiRest/Controllers/UsuarioController.java
+++ apiRest/src/main/java/com/apiResrt/apiRest/Controllers/UsuarioController.java
@@ -1,4 +1,25 @@
 package com.apiResrt.apiRest.Controllers;
 
+import com.apiResrt.apiRest.Models.UsuarioModel;
+import com.apiResrt.apiRest.Services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/")
 public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+
+    @PostMapping
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
 }
--- apiRest/src/main/java/com/apiResrt/apiRest/Services/UsuarioService.java
+++ apiRest/src/main/java/com/apiResrt/apiRest/Services/UsuarioService.java
@@ -1,4 +1,22 @@
 package com.apiResrt.apiRest.Services;
 
+import com.apiResrt.apiRest.Models.UsuarioModel;
+import com.apiResrt.apiRest.Repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
 public class UsuarioService {
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return (ArrayList<UsuarioModel>)usuarioRepository.findAll();
+    }
+
+    public UsuarioModel guardarUsuario(UsuarioModel usuario){
+        return usuarioRepository.save(usuario);
+    }
 }
